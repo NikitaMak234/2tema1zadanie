b7ada7902982ec3ed43eaefa5f0a8f77b0040fbc
NikitaMak234 <makarovnikita020395@yandex.ru>
2021-12-06 23:18:28 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amountInKopecks = 10000;
+        int percentInKopecks = 20;
+        int bonus = amountInKopecks / percentInKopecks;
+        System.out.println(" Your bonus is:"+ bonus +" miles");
+
+    }
+}
